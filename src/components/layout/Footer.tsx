1e4ffe80eb40f52d4b022dec0fc77b7af8c8caa9
Reza Saputra <rezagondol@gmail.com>
2025-06-08 20:11:41 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,16 +15,23 @@ export function Footer() {
               <Image 
                 src="./images/amayya.svg" 
                 alt="Amàyyà Tour Logo" 
-                data-ai-hint="logo compass"
                 width={32} 
                 height={32} 
                 className="text-accent" 
               />
-              <span className="font-headline font-bold text-2xl">Amàyyà Tour</span>
+              <span className="font-headline font-bold text-2xl">PT AMAYYA LINTAS AMANI</span>
             </Link>
             <p className="text-sm text-primary-foreground/80">
               Perjalanan ibadah Umrah Anda adalah prioritas kami. Amàyyà Tour berkomitmen memberikan pelayanan terbaik untuk pengalaman spiritual yang tak terlupakan.
             </p>
+            <div className="text-sm hover:text-accent transition-colors flex flex-col items-start mt-4">
+              <Image 
+                src="/images/terdaftar.svg"
+                width={250} 
+                height={100}
+                alt="terdaftar di"
+              />
+          </div>
           </div>
 
           <div>
@@ -32,15 +39,15 @@ export function Footer() {
             <ul className="space-y-2 text-sm">
               <li className="flex items-start">
                 <MapPin className="h-5 w-5 mr-2 mt-0.5 text-accent flex-shrink-0" />
-                <span>Jl. Kebenaran No. 1, Jakarta Pusat, Indonesia</span>
+                <span>Jl. Lodaya No.11 A, Malabar, Kec. Lengkong, Kota Bandung, Jawa Barat 40232</span>
               </li>
               <li className="flex items-center">
                 <Phone className="h-5 w-5 mr-2 text-accent flex-shrink-0" />
-                <a href="tel:+62211234567" className="hover:text-accent transition-colors">+62 21 1234 567</a>
+                <a href="https://wa.me/6281319463463" className="hover:text-accent transition-colors">+62 813 1946 3463</a>
               </li>
               <li className="flex items-center">
                 <Mail className="h-5 w-5 mr-2 text-accent flex-shrink-0" />
-                <a href="mailto:info@amayyatour.com" className="hover:text-accent transition-colors">info@amayyatour.com</a>
+                <a href="mailto:amayyatour@gmail.com" className="hover:text-accent transition-colors">amayyatour@gmail.com</a>
               </li>
             </ul>
           </div>
@@ -48,13 +55,13 @@ export function Footer() {
           <div>
             <h3 className="text-lg font-semibold mb-4 text-accent font-headline">Ikuti Kami</h3>
             <div className="flex space-x-4">
-              <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Facebook Amàyyà Tour" className="text-primary-foreground/80 hover:text-accent transition-colors">
+              <a href="https://www.facebook.com/profile.php?id=61554284753205&locale=id_ID" target="_blank" rel="noopener noreferrer" aria-label="Facebook Amàyyà Tour" className="text-primary-foreground/80 hover:text-accent transition-colors">
                 <Facebook className="h-6 w-6" />
               </a>
               <a href="https://www.instagram.com/amayyatour.id/" target="_blank" rel="noopener noreferrer" aria-label="Instagram Amàyyà Tour" className="text-primary-foreground/80 hover:text-accent transition-colors">
                 <Instagram className="h-6 w-6" />
               </a>
-              <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Youtube Amàyyà Tour" className="text-primary-foreground/80 hover:text-accent transition-colors">
+              <a href="https://www.youtube.com/@amayyatour2495" target="_blank" rel="noopener noreferrer" aria-label="Youtube Amàyyà Tour" className="text-primary-foreground/80 hover:text-accent transition-colors">
                 <Youtube className="h-6 w-6" />
               </a>
             </div>
